e6887e4caed29978e17c6de1c660f1c4fa907f21
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2022-05-08 18:40:44 +0700

    Import existing project
    Initial settings to inventory system, hard coding UI.

---
+++ Own Inventory Test/Assets/Script/UI/UIInventoryDescription.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventoryDescription : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text quantityText;
+    [SerializeField] private TMP_Text itemName;
+    [SerializeField] private TMP_Text itemDescription;
+
+    private void Awake() {
+        ResetDescription();
+    }
+
+    public void ResetDescription()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        this.quantityText.text = "";
+        this.itemName.text = "";
+        this.itemDescription.text = "";
+    }
+
+    public void SetDescription(Sprite sprite, int quantity, string name, string description)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityText.text = quantity + "";
+        this.itemName.text = name;
+        this.itemDescription.text = description;
+    }
+}
---
+++ Own Inventory Test/Assets/Script/UI/UIInventoryPage.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField] private UIInventoryItem itemPrefab;
+
+    [SerializeField] private RectTransform contentPanel;
+
+    [SerializeField] private UIInventoryDescription itemDescription;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public Sprite image;
+    public int quantity;
+    public string nameItem;
+    public string descriptionItem;
+
+    private void Awake() {
+        Hide();
+        itemDescription.ResetDescription();
+    }
+    public void InitializeInventoryUI(int inventorySize)
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel, false);
+            listOfUIItems.Add(uiItem);
+
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRIghtMouseBtnClick += HandleShowItemActions;
+        }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+        Debug.Log("Show item actions");
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+        Debug.Log("End Drag");
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+        Debug.Log("Swap");
+    }
+
+    private void HandleBeginDrag(UIInventoryItem obj)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    private void HandleItemSelection(UIInventoryItem obj)
+    {
+        itemDescription.SetDescription(image, quantity, nameItem, descriptionItem);
+        listOfUIItems[0].Select();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+
+        listOfUIItems[0].SetData(image, quantity);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
